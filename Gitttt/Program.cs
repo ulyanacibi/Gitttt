6d6aff3b2566938babcd6e4cb874b20ac3c37861
Ульяна Чикибряк < @DESKTOP-P85QI9V>
2024-02-26 21:26:13 +0300

    Добавьте файлы проекта.

---
+++ Gitttt/Program.cs
@@ -0,0 +1,8 @@
+﻿int x;
+Console.WriteLine("Введите число x");
+x=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine($"y = {3 - x}");
+Console.WriteLine(Math.Pow(3, x));
+Console.WriteLine(Math.Sqrt(x));
+Console.WriteLine(Math.Log10(x));
+
